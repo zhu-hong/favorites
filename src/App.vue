cc42034e5d4819da162bcc93c686be8604748b8f
zhu-hong <zhxhyy@qq.com>
2022-10-29 15:11:52 +0800

    添加配置

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import ThemeToggler from './components/ThemeToggler.vue'
 import { useFavStore } from './store'
-import FilterInput from './components/FilterInput.vue';
-import Favorite from './components/Favorite.vue';
+import FilterInput from './components/FilterInput.vue'
+import Favorite from './components/Favorite.vue'
+import { CodeBlock } from '@zhu-hong/code-block'
+import '@zhu-hong/code-block/style'
 
 const favStore = useFavStore()
 </script>
@@ -15,5 +17,11 @@ const favStore = useFavStore()
 
   <div class="grid grid-cols-5 gap-6 at-lg:grid-cols-4 at-md:grid-cols-3 at-sm:grid-cols-2 lt-sm:grid-cols-1">
     <Favorite :f="f" v-for="f of favStore.favoritesFilter" />
+  </div>
+
+  <div class="font-medium text-xl mt-10">配置</div>
+  <div v-for="f of favStore.configs" class="not-last:mt-5">
+    <div class="my-2">{{ f.name }}</div>
+    <CodeBlock :code="f.value"/>
   </div>
 </template>
